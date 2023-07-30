ef19b159803a01e7586f85c3bd9f46afeec70779
Yogesh <awesomeyogi10@gmail.com>
2023-07-30 21:33:29 +0530

    shifted import os to top in settings.py

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 ]
 LOGIN_URL = 'two_factor:login'
 
-import os
+
 
 key_file_path = os.path.join(BASE_DIR, 'key.txt')
 with open(key_file_path, 'r') as key_file:
