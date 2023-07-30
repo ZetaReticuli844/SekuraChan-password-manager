ca0700f0a6297a585595fc573a374ad3256d6df2
Yogesh <awesomeyogi10@gmail.com>
2023-07-30 12:03:15 +0530

    added vercel files

--- passwordmanager/passwordmanager/settings.py
+++ passwordmanager/passwordmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6nddgjf1877)vr3--qku*7dij)=%1whj=ac&qmm$(c=*90(!^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,17 +83,17 @@ WSGI_APPLICATION = 'passwordmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SekuraDataBase',
-        'USER':'postgres',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'5432',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'SekuraDataBase',
+#         'USER':'postgres',
+#         'PASSWORD':'y0g@$#10',
+#         'HOST':'localhost',
+#         'PORT':'5432',
         
-    }
-}
+#     }
+# }
 
 
 # Password validation
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
