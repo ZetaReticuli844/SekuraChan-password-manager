772347d2f0be380c641d424f4f7576c6fdb1dd11
Yogesh <awesomeyogi10@gmail.com>
2023-08-03 11:12:43 +0530

    replace postgres with sql lite due to issues

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -83,15 +83,21 @@ WSGI_APPLICATION = 'passwordmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'SekuraDataBase',
+#         'USER':'yogeshshekhawat',
+#         'PASSWORD':'yogesh10',
+#         'HOST':'database-1.csfjaizjtatp.us-east-1.rds.amazonaws.com',
+#         'PORT':'5432',
+        
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SekuraDataBase',
-        'USER':'yogeshshekhawat',
-        'PASSWORD':'yogesh10',
-        'HOST':'database-1.csfjaizjtatp.us-east-1.rds.amazonaws.com',
-        'PORT':'5432',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
