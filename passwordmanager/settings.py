5594df5349e66aba0deabfd47c36f46e24876ad4
ZetaReticuli844 <108940702+ZetaReticuli844@users.noreply.github.com>
2023-12-11 11:04:53 +0530

    Update settings.py

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -82,16 +82,14 @@ WSGI_APPLICATION = 'passwordmanager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SekuraDataBase',
-        'USER':'yogeshshekhawat',
-        'PASSWORD':'yogesh10',
-        'HOST':'database-1.csfjaizjtatp.us-east-1.rds.amazonaws.com',
-        'PORT':'5432',
-        
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
